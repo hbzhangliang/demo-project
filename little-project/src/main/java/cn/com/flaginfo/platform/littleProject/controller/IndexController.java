6c9fc65a9895239cb52a8decfc60fc0580549d12
zhangliang <avonny@126.com>
2019-04-04 17:17:36 +0800

    #STORY 测试接口

--- little-project/src/main/java/cn/com/flaginfo/platform/littleProject/controller/IndexController.java
+++ little-project/src/main/java/cn/com/flaginfo/platform/littleProject/controller/IndexController.java
@@ -1,12 +1,17 @@
 package cn.com.flaginfo.platform.littleProject.controller;
 
 import cn.com.flaginfo.platform.littleProject.mongo.models.Dict;
+import cn.com.flaginfo.platform.littleProject.mongo.models.MetroLine;
 import cn.com.flaginfo.platform.littleProject.mongo.repo.DictRepo;
+import cn.com.flaginfo.platform.littleProject.mongo.repo.MetroLineRepo;
 import cn.com.flaginfo.platform.littleProject.utils.RedisUtils;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping(value = "/")
 @RestController
@@ -44,6 +49,32 @@ public class IndexController {
         }
         return dictRepo.listAll();
     }
+
+    @RequestMapping(value = "/metro")
+    public Object metro(){
+        List<MetroLine> list=new ArrayList<>();
+        for(int i=0;i<36;i++){
+            MetroLine tmp=new MetroLine();
+            if(i<12) {
+                tmp.setCity("上海");
+            }
+            else if(i<22){
+                tmp.setCity("北京");
+            }
+            else {
+                tmp.setCity("武汉");
+            }
+            tmp.setName("地铁"+i+"号线");
+            list.add(tmp);
+        }
+        metroLineRepo.save(list);
+        return metroLineRepo.list();
+    }
+
+
+
+    @Autowired
+    private MetroLineRepo metroLineRepo;
 
     @Autowired
     private DictRepo dictRepo;
